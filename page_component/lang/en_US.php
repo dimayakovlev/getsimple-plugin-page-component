d1746d779bdf3cfdaa4a968ea4342347a197e59f
Дмитрий Яковлев <web-dimayakovlev@outlook.com>
2016-07-22 19:35:55 +0300

    Fix plugin description

--- page_component/lang/en_US.php
+++ page_component/lang/en_US.php
@@ -2,7 +2,7 @@
 $i18n = array(
   'AUTHOR' => 'Dmitry Yakovlev',
   'TITLE' => 'Page Component',
-  'PLUGIN_DESCRIPTION' => 'Add components for pages',
+  'DESCRIPTION' => 'Add components for pages',
   'TAB_TITLE' => 'Component',
   'PAGE_COMPONENT_ENABLE' => 'Enable page component',
 );
